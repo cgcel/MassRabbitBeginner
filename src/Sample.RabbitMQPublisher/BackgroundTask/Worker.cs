e8545e221a46a1d27fc96aca2792c494b1517e1f
cgcel <cgc.elvin@outlook.com>
2023-05-23 21:26:17 +0800

    添加项目文件。

---
+++ src/Sample.RabbitMQPublisher/BackgroundTask/Worker.cs
@@ -0,0 +1,34 @@
+﻿using MassTransit;
+using Sample.RabbitMQPublisher.Model;
+using Microsoft.Extensions.Hosting;
+
+namespace Sample.RabbitMQPublisher.BackgroundTask
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IBus _bus;
+        private readonly DataPublishConfig _config;
+
+        public Worker(IBus bus, DataPublishConfig config)
+        {
+            _bus = bus;
+            _config = config;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _bus.Publish<MetaStringModel>(new MetaStringModel
+                {
+                    Text = "Hello World"
+                }, c =>
+                {
+                    c.SetRoutingKey(_config.RoutingKey);
+                }, stoppingToken);
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ src/Sample.RabbitMQPublisher/README.md
@@ -0,0 +1,6 @@
+# Sample.RabbitMQPublisher
+
+## About
+
+Send data to a predefine queue by using vhost, exchange name, routing key.
+

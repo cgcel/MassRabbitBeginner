79ce2bf7770b49a3b95a3dfa3c6fffb45f4a8a36
cgcel <cgc.elvin@outlook.com>
2023-05-31 08:37:16 +0800

    add: simple consumer.

---
+++ src/Sample.SimpleConsumer/Model/MetaModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Sample.SimpleConsumer.Model
+{
+    public class MetaBaseModel
+    {
+        public string type { get; set; }
+        public string st { get; set; }
+        public string sender { get; set; }
+    }
+
+    public class MetaGeneralModel
+    {
+        public MetaBaseModel meta { get; set; }
+        public dynamic data { get; set; }
+    }
+
+    public class MetaStringModel
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ src/Sample.SimpleConsumer/Program.cs
@@ -0,0 +1,64 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MassTransit;
+using Sample.SimpleConsumer.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+try
+{
+    var app = CreateHostBuilder(args).Build();
+    await app.RunAsync();
+}
+catch (Exception)
+{
+
+    throw;
+}
+
+IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>((hostContext, builder) =>
+    {
+        builder.Register(c =>
+        {
+            var setting = hostContext.Configuration.GetSection(nameof(RabbitMqConfig)).GetSection((nameof(DataSubscribeConfig))).Get<DataSubscribeConfig>();
+            return setting;
+        }).As<DataSubscribeConfig>();
+    })
+    .ConfigureServices((hostContext, service) =>
+    {
+        service.AddMassTransit(cfg =>
+        {
+            var setting = hostContext.Configuration.GetSection(nameof(RabbitMqConfig)).GetSection(nameof(DataSubscribeConfig)).Get<DataSubscribeConfig>();
+            cfg.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(setting.Host, setting.VirtualHost, cfg =>
+                {
+                    cfg.Username(setting.UserName);
+                    cfg.Password(setting.Password);
+                });
+
+                cfg.Message<MetaStringModel>(x => x.SetEntityName(setting.ExchangeName));
+
+                cfg.Publish<MetaStringModel>(e =>
+                {
+                    e.Durable = true;
+                    e.ExchangeType = "topic";
+                    e.AutoDelete = false;
+                    e.BindQueue(setting.ExchangeName, setting.QueueName, c =>
+                    {
+                        c.Durable = true;
+                        c.AutoDelete = false;
+                        c.ExchangeType = "topic";
+                        c.RoutingKey = setting.RoutingKey;
+                        c.SetQueueArgument("x-message-ttl", 259200000);
+                    });
+                });
+
+            });
+        });
+
+        //service.AddHostedService<Worker>();
+    });
